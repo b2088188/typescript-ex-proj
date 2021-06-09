2189c78be0a985a217bd38c3cc400358248af3ee
Shunze Lin <4A490101@stust.edu.tw>
2021-06-09 15:24:55 +0800

    feat:Defining initial type of User

---
+++ src/User.ts
@@ -0,0 +1,10 @@
+import faker from 'faker';
+
+function User(this:{name:string,location:{lat:number, lng:number}}) {
+  this.name = faker.name.firstName();
+  this.location = {
+    lat:+faker.address.latitude(),
+    lng:+faker.address.longtiude()
+  }
+}
+
