150def7334f6918567d5bec90ea1ec63141ac7bc
Shunze Lin <4A490101@stust.edu.tw>
2021-06-09 17:10:42 +0800

    feat:Implementing adding user marker

--- src/User.ts
+++ src/User.ts
@@ -1,6 +1,12 @@
 import faker from 'faker';
 
-function User(this:{name:string,location:{lat:number, lng:number}}) {
+function User(this:{
+  name:string,
+  location:{
+    lat:number, 
+    lng:number
+  }
+}) {
   this.name = faker.name.firstName();
   this.location = {
     lat:+faker.address.latitude(),
